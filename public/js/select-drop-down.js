b6f909d43d39f89582cb33dac22cb5770e5f3dc0
shakirr1176 <shakir@appolay.com>
2025-01-09 23:17:54 +0600

    dropdown update

--- public/js/select-drop-down.js
+++ public/js/select-drop-down.js
@@ -1,121 +1,68 @@
-{
-    function dropDownFun(selectWrapper) {
-        var x, i, j, l, ll, selElmnt, a, b, c;
-        x = selectWrapper;
-        l = x.length;
-        for (i = 0; i < l; i++) {
-            let selectTtems = x[i].querySelector(".select-items");
-
-            if (selectTtems) {
-                selectTtems.remove();
-            }
-
-            let selectSelected = x[i].querySelector(".select-selected");
-
-            if (selectSelected) {
-                selectSelected.remove();
-            }
-
-            if (x[i].querySelector(".select-items") == null) {
-                selElmnt = x[i].getElementsByTagName("select")[0];
-                ll = selElmnt.length;
-                a = document.createElement("DIV");
-                a.setAttribute("class", "select-selected");
-
-                a.innerHTML =
-                    selElmnt.options[selElmnt.selectedIndex].innerHTML;
-                x[i].appendChild(a);
-                b = document.createElement("ul");
-                b.setAttribute("class", "select-items select-hide");
-                for (j = 1; j < ll; j++) {
-                    c = document.createElement("li");
-                    c.innerHTML = selElmnt.options[j].innerHTML;
-                    c.setAttribute("data-value", selElmnt.options[j].value);
-                    c.removeAttribute("class");
-
-                    if (selElmnt.value == selElmnt.options[j].value) {
-                        c.setAttribute("class", "same-as-selected active");
-                    }
-
-                    c.addEventListener("click", function (e) {
-                        var y, i, k, s, h, sl, yl;
-                        s =
-                            this.parentNode.parentNode.getElementsByTagName(
-                                "select"
-                            )[0];
-                        sl = s.length;
-                        h = this.parentNode.previousSibling;
-                        for (i = 0; i < sl; i++) {
-                            if (s.options[i].value == this.dataset["value"]) {
-                                s.selectedIndex = i;
-                                h.innerHTML = this.innerHTML;
-                                y =
-                                    this.parentNode.getElementsByClassName(
-                                        "same-as-selected"
-                                    );
-                                yl = y.length;
-                                for (k = 0; k < yl; k++) {
-                                    y[k].removeAttribute("class");
-                                }
-                                this.setAttribute(
-                                    "class",
-                                    "same-as-selected active"
-                                );
-                                break;
-                            }
-                        }
-                        h.click();
-                    });
-                    b.appendChild(c);
-                }
-                x[i].appendChild(b);
-                a.addEventListener("click", function (e) {
-                    closeControl(this);
-                    this.nextSibling.classList.toggle("select-hide");
-                    this.classList.toggle("select-arrow-active");
-                });
-            }
-        }
+function dropDownFun(selectWrapper) {
+    if (selectWrapper.querySelector(".select-selected")) return;
+  
+    let selElmnt = selectWrapper.querySelector("select");
+    let ll = selElmnt.length;
+  
+    let a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    selectWrapper.appendChild(a);
+  
+    let b = document.createElement("ul");
+    b.setAttribute("class", "select-items select-hide");
+  
+    for (let j = 0; j < ll; j++) {
+      let c = document.createElement("li");
+      c.innerHTML = selElmnt.options[j].innerHTML;
+      c.dataset.value = selElmnt.options[j].value;
+      if (selElmnt.value === selElmnt.options[j].value) {
+        c.classList.add("same-as-selected", "active");
+      }
+  
+      c.addEventListener("click", function () {
+        selElmnt.selectedIndex = [...selElmnt.options].findIndex(
+          (opt) => opt.value === this.dataset.value
+        );
+        a.innerHTML = this.innerHTML;
+        b.querySelectorAll(".same-as-selected").forEach((el) =>
+          el.classList.remove("same-as-selected", "active")
+        );
+        this.classList.add("same-as-selected", "active");
+        a.click();
+      });
+  
+      b.appendChild(c);
     }
-
-    function closeAllSelect(elmnt) {
-        if (!elmnt.target.classList.contains("select-selected")) {
-            closeControl(elmnt);
-        }
-    }
-
-    function closeControl(elmnt) {
-        var x,
-            y,
-            i,
-            xl,
-            yl,
-            arrNo = [];
-        x = document.getElementsByClassName("select-items");
-        y = document.getElementsByClassName("select-selected");
-        xl = x.length;
-        yl = y.length;
-
-        for (i = 0; i < yl; i++) {
-            if (elmnt == y[i]) {
-                arrNo.push(i);
-            } else {
-                y[i].classList.remove("select-arrow-active");
-            }
-        }
-
-        for (i = 0; i < xl; i++) {
-            if (arrNo.indexOf(i)) {
-                x[i].classList.add("select-hide");
-            }
-        }
-    }
-    document.addEventListener("click", closeAllSelect);
-
-    window.addEventListener("load", () => {
-        let selectWrapper = document.querySelectorAll(".custom-select");
-        dropDownFun(selectWrapper);
+  
+    selectWrapper.appendChild(b);
+  
+    a.addEventListener("click", function () {
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
     });
-
-    window.dropDownFun = dropDownFun;
-}
+  }
+  
+  function closeAllSelect(event) {
+    document.querySelectorAll(".select-selected").forEach((sel) => {
+      if (sel !== event) sel.classList.remove("select-arrow-active");
+    });
+  
+    document.querySelectorAll(".select-items").forEach((item) => {
+      if (!item.previousSibling.isSameNode(event)) {
+        item.classList.add("select-hide");
+      }
+    });
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".custom-select").forEach(dropDownFun);
+  });
+  
+  document.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("select-selected")) {
+      closeAllSelect(e.target);
+    }
+  });
+  
